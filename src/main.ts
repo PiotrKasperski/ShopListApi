691b34bc99e418575dc5e78464db59d206393145
Piotr Kasperski <piotr.kasperski94@gmail.com>
2024-08-17 16:10:28 +0200

    Auto-commit changes in submodule from branch 6-hide-status-bar

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:8100',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
